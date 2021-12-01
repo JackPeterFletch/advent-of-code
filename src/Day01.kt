30cbb9839027367d4ffd2d5f42735cbd6546b902
Jack Fletcher <jack_p_fletcher@apple.com>
2021-12-01 13:55:08 +0000

    Advent of Code - Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,17 @@
 fun main() {
     // Part 1
-    readIntInput("Day01_test") getTotalDepthIncreases { check(this == 7) }
-    readIntInput("Day01") getTotalDepthIncreases ::println
+    readIntInput("Day01_test")
+        .getTotalDepthIncreases { check(this == 7) }
+    readIntInput("Day01")
+        .getTotalDepthIncreases(::println)
 
     // Part 2
-    readIntInput("Day01_test").withSlidingWindows() getTotalDepthIncreases { check(this == 5) }
-    readIntInput("Day01").withSlidingWindows() getTotalDepthIncreases ::println
+    readIntInput("Day01_test")
+        .withSlidingWindows()
+        .getTotalDepthIncreases { check(this == 5) }
+    readIntInput("Day01")
+        .withSlidingWindows()
+        .getTotalDepthIncreases(::println)
 }
 
 private fun List<Int>.withSlidingWindows() =
@@ -13,7 +19,7 @@ private fun List<Int>.withSlidingWindows() =
         if(index + 2 < size){ depth + get(index+1) + get(index+2) } else { null }
     }.filterNotNull()
 
-private infix fun List<Int>.getTotalDepthIncreases(block: Int.() -> Unit) =
+private fun List<Int>.getTotalDepthIncreases(block: Int.() -> Unit = {}) =
     mapIndexed { index, depth ->
         if (getPreviousDepth(index, depth) >= depth) { 0 } else { 1 }
     }.sum().block()
